95ca2ee6646282a541041020dd4bee79fadb4766
maxarvid <max@klimato.com>
2025-02-12 20:21:27 +0100

    Adds Ollama dependency and dummy test endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import ollama from 'ollama'
 
 const app = new Hono()
 
 app.get('/', (c) => {
   return c.text('Hello Seekr!')
+})
+
+app.get('/ollama', async (c) => {
+  const testMessage = "Hello Ollama! How are you?"
+
+  const streamResponse = await ollama.chat({
+    model: 'deepseek-r1:1.5b',
+    messages: [{ role: 'user', content: testMessage }]
+  });
+
+  const responseText = streamResponse.message.content
+
+  return c.text(responseText)
 })
 
 const port = 3000
